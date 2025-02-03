9fdae9d2cc7e3c748bd4c88aa9ba6c2403d2b760
Sofienne Lassoued <sofienne@hexis.live>
2025-02-03 11:54:14 +0100

    Added users CRUD

--- src/views/users/details/index.vue
+++ src/views/users/details/index.vue
@@ -1,11 +1,147 @@
 <template>
-  <div>
-    User details
+  <!-- Breadcrumb -->
+  <nav aria-label="breadcrumb" class="main-breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><router-link to="/">Home</router-link></li>
+      <li class="breadcrumb-item"><router-link to="/users">Users</router-link></li>
+      <li class="breadcrumb-item active" aria-current="page">Profile</li>
+    </ol>
+  </nav>
+  <!-- /Breadcrumb -->
+  <div class="row gutters-sm" v-if="!loading && payload">
+    <div class="col-md-4 mb-3">
+      <div class="card">
+        <div class="card-body">
+          <div class="d-flex flex-column align-items-center text-center">
+            <img :src="payload.image" alt="Admin" class="rounded-circle" width="150">
+            <div class="mt-3">
+              <h4>{{ payload.name }}</h4>
+              <router-link class="btn btn-primary" :to="'/offers?user=' + id">View offers</router-link>
+              <span class="mx-1"></span>
+              <router-link class="btn btn-secondary" :to="'/offers/new?user=' + id">Create offer</router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-8">
+      <div class="card mb-3">
+        <div class="card-body">
+          <div class="row">
+            <div class="col-sm-3">
+              <h6 class="mb-0">Name</h6>
+            </div>
+            <div class="col-sm-9 text-secondary">
+              {{ payload.name }}
+            </div>
+          </div>
+          <hr>
+          <div class="row">
+            <div class="col-sm-3">
+              <h6 class="mb-0">Email</h6>
+            </div>
+            <div class="col-sm-9">
+              <a class="text-primary" :href="'mailto:' + payload.email">{{ payload.email }}</a>
+            </div>
+          </div>
+          <hr>
+          <div class="row">
+            <div class="col-sm-3">
+              <h6 class="mb-0">Representative</h6>
+            </div>
+            <div class="col-sm-9 text-secondary">
+              {{ payload.representative }}
+            </div>
+          </div>
+          <hr>
+          <div class="row">
+            <div class="col-sm-3">
+              <h6 class="mb-0">Website</h6>
+            </div>
+            <div class="col-sm-9 text-secondary">
+              <a :href="payload.website" target="_blank" class="text-primary">{{ payload.website }}</a>
+            </div>
+          </div>
+          <hr>
+          <div class="row">
+            <div class="col-sm-3">
+              <h6 class="mb-0">Contact Email</h6>
+            </div>
+            <div class="col-sm-9 text-secondary">
+              <a :href="'mailto:' + payload.contactEmail" target="_blank" class="text-primary">{{ payload.contactEmail
+                }}</a>
+            </div>
+          </div>
+          <hr>
+          <div class="row">
+            <div class="col-sm-3">
+              <h6 class="mb-0">Phone</h6>
+            </div>
+            <div class="col-sm-9 text-secondary">
+              <a :href="'tel:' + payload.phone" target="_blank" class="text-primary">{{ payload.phone }}</a>
+            </div>
+          </div>
+          <hr>
+          <div class="row">
+            <div class="col-sm-3">
+              <h6 class="mb-0">Instagram</h6>
+            </div>
+            <div class="col-sm-9 text-secondary">
+              <a :href="payload.instagram" target="_blank" class="text-primary">{{ payload.instagram }}</a>
+            </div>
+          </div>
+          <hr>
+          <div class="row">
+            <div class="col-sm-3">
+              <h6 class="mb-0">Facebook</h6>
+            </div>
+            <div class="col-sm-9 text-secondary">
+              <a :href="payload.facebook" target="_blank" class="text-primary">{{ payload.facebook }}</a>
+            </div>
+          </div>
+          <hr>
+          <div class="row">
+            <div class="col-sm-3">
+              <h6 class="mb-0">WhatsApp</h6>
+            </div>
+            <div class="col-sm-9 text-secondary">
+              <a :href="'tel:' + payload.whatsapp" target="_blank" class="text-primary">{{ payload.whatsapp }}</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="vh-100 d-flex justify-content-center align-items-center" v-if="loading">
+    <div class="text-center">
+      <div class="spinner-border" role="status">
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-
+import { getUserMetadata } from '@/domain/users';
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+const route = useRoute()
+const loading = ref(false)
+const payload = ref()
+const id = ref()
+onMounted(() => {
+  id.value = route.params.id as string
+  const fn = async () => {
+    try {
+      loading.value = true
+      payload.value = await getUserMetadata(id.value)
+      loading.value = false
+    } catch (error) {
+      loading.value = false
+      console.log(error)
+    }
+  }
+  fn()
+})
 </script>
 
 <style scoped></style>
--- src/views/users/home/index.vue
+++ src/views/users/home/index.vue
@@ -1,11 +1,90 @@
 <template>
-  <div>
-    Users
+  <!-- Breadcrumb -->
+  <div class="d-flex justify-content-between">
+    <nav aria-label="breadcrumb" class="main-breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><router-link to="/">Home</router-link></li>
+        <li class="breadcrumb-item active" aria-current="page">Users</li>
+      </ol>
+    </nav>
+  </div>
+  <div class="album py-5 bg-body-tertiary" v-if="!loading && items.length > 0">
+    <div class="container">
+      <div class="row">
+        <div class="card p-3">
+          <div class="d-flex justify-content-between">
+            <div>
+              <search :items="allItems" @filtered="handleOnFiltered" />
+            </div>
+          </div>
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Logo</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in items" :key="item.id">
+                <th scope="row"><img class="image" :src="item.image" /></th>
+                <td>{{ item.name }}</td>
+                <td>{{ item.email }}</td>
+                <td>
+                  <router-link type="button" class="btn btn-sm btn-outline-primary mx-2"
+                    :to="'/users/' + item.id">View</router-link>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="vh-100 d-flex justify-content-center align-items-center" v-if="!loading && items.length === 0">
+    <div class="text-center">
+      <p>No items</p>
+    </div>
+  </div>
+  <div class="vh-100 d-flex justify-content-center align-items-center" v-if="loading">
+    <div class="text-center">
+      <div class="spinner-border" role="status">
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-
+import Search from '@/components/miscs/forms/search/index.vue';
+import { getUsers } from '@/domain/users';
+import { onMounted, ref } from 'vue';
+const loading = ref(false)
+const allItems = ref([])
+const items = ref([])
+onMounted(() => {
+  const fn = async () => {
+    try {
+      loading.value = true
+      allItems.value = await getUsers()
+      items.value = allItems.value;
+      loading.value = false
+    } catch (error) {
+      loading.value = false
+      console.log(error)
+    }
+  }
+  fn()
+})
+const handleOnFiltered = (i) => {
+  items.value = i
+}
 </script>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+.image {
+  height: 32px;
+  width: 32px;
+  border-radius: 16px;
+}
+</style>
